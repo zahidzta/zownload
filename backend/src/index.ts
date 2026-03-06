98f90c68c144e70d866ea141afe368ed5e5e55b2
zahid <zahidduron@gmail.com>
2026-03-06 00:27:32 -0600

    refactor: standardize code formatting and improve readability in index.ts

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,30 +1,31 @@
-import express, { Application, Request, Response } from 'express';
-import { createServer } from 'http';
-import { Server, Socket } from 'socket.io';
-import cors from 'cors';
+import express, { Application, Request, Response } from "express"
+import cors from "cors"
+import { createServer } from "http"
+import { Server, Socket } from "socket.io"
 
-const app: Application = express();
-app.use(cors());
+const app : Application = express()
+app.use(cors())
 
-const httpServer = createServer(app);
+const httpServer = createServer(app)
+
 const io = new Server(httpServer, {
     cors: { origin: "*" }
-});
+})
 
-io.on('connection', (socket: Socket) => {
-    console.log('connected', socket.id);
-    
-    socket.on('disconnect', () => {
-        console.log('disconnected');
-    });
-});
+io.on("connection", (socket : Socket) => {
+    console.log("connected user: ", socket.id)
 
-app.get('/', (req: Request, res: Response) => {
-    res.send('connected server');
-});
+    socket.on("disconnect", () => {
+        console.log("disconnected")
+    })
+})
 
-const PORT = process.env.PORT || 4000;
+app.get("/", (req : Request, res : Response) => {
+    res.send("connected server")
+})
+
+const PORT = process.env.PORT || 4000
 
 httpServer.listen(PORT, () => {
-    console.log(`Server running in http://localhost:${PORT}`);
-});
+    console.log(`server running in http://localhost:${PORT}`)
+})
